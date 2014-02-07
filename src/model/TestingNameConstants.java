34e43656c304a3bb8d2ed9f383febe2a66811c3c
Ricky <rickygorowsky@email.arizona.edu>
2014-02-07 13:06:01 -0700

    forgot some more things

---
+++ src/model/TestingNameConstants.java
@@ -0,0 +1,21 @@
+package model;
+
+
+/**
+ * Holds all the common names to be used in the output file.
+ * @author Ricky
+ */
+public class TestingNameConstants {
+    /**
+     * The default name for a return value.
+     */
+    public static final String RETURN_VALUE = "returnValue";
+
+
+    /**
+     * private constructor so nobody can instantiate this class.
+     */
+    private TestingNameConstants() {
+
+    }
+}
