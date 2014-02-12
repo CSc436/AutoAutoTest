f0d7718d8ff76c4ce022d6b5a5dc0af8b09966e3
Ricky <rickygorowsky@email.arizona.edu>
2014-02-12 15:00:50 -0700

    Finish the Args wrapper class with some basic tests.

---
+++ src/model/Args.java
@@ -0,0 +1,45 @@
+package model;
+
+
+/**
+ * wrapper class for Args to be passed into a method
+ * @author Ricky
+ *
+ */
+public class Args {
+
+    private String args;
+    
+    /**
+     * default constructor, sets args to "".
+     */
+    public Args() {
+        args = "";
+    }
+    
+    /**
+     * @param val sets args to val.
+     */
+    public Args(String val) {
+        args = val;
+    }
+    
+    /**
+     * @return returns args.
+     */
+    public String getArgsValue() {
+        return args;
+    }
+    
+    /**
+     * @param val set args to value
+     */
+    public void setArgsValue(String val) {args = val;}
+    
+    /**
+     * Just prints args.
+     */
+    public String toString() {
+        return args;
+    }
+}
---
+++ src/model/ArgsTest.java
@@ -0,0 +1,26 @@
+package model;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ArgsTest {
+
+    /**
+     * very basic tests of the Args wrapper class.
+     */
+    @Test
+    public void ArgsTest() {
+
+            Args args = new Args();
+            assertTrue(args.getArgsValue().equals(""));
+
+            args.setArgsValue("x, false, 3.14");
+            assertTrue(args.getArgsValue().equals("x, false, 3.14"));
+            
+            args = new Args("\"indubitably\"");
+            assertTrue(args.getArgsValue().equals("\"indubitably\""));
+            
+            assertTrue(args.toString().equals(args.getArgsValue()));
+    }
+}
