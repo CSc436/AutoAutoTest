3b0956899fd1bf7314f02b18f17e6cc52d508d99
Dillon Jeffers <jeffersd@email.arizona.edu>
2014-02-13 16:58:03 -0700

    I started coding the design portion in fxml, and will be doing further conversions later. The gui currently works.

---
+++ src/ViewController.java
@@ -0,0 +1,16 @@
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+ 
+public class ViewController {
+	private int testcounter = 0;
+    @FXML private Text actiontarget;
+    
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        actiontarget.setText("Test " + testcounter + "Generated.");
+        testcounter++;
+    }
+
+}
