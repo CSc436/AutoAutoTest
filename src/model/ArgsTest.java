5f0117ad5aafb2af15298cb641185a843bac48d3
Ricky <rickygorowsky@email.arizona.edu>
2014-02-13 15:47:31 -0700

    made the changes requested in code review

--- src/model/ArgsTest.java
+++ src/model/ArgsTest.java
@@ -7,20 +7,41 @@ import org.junit.Test;
 public class ArgsTest {
 
     /**
-     * very basic tests of the Args wrapper class.
+     * Test empty constructor.
      */
     @Test
-    public void ArgsTest() {
-
+    public void ArgsEmptyConstructorTest() {
         Args args = new Args();
         assertTrue(args.getArgsValue().equals(""));
+    }
+
+    /**
+     * Test settter.
+     */
+    @Test
+    public void ArgsSetterTest() {
+        Args args = new Args();
 
         args.setArgsValue("x, false, 3.14");
         assertTrue(args.getArgsValue().equals("x, false, 3.14"));
-
-        args = new Args("\"indubitably\"");
-        assertTrue(args.getArgsValue().equals("\"indubitably\""));
-
-        assertTrue(args.toString().equals(args.getArgsValue()));
     }
+
+    /**
+     * Test parameterized constructor.
+     */
+    @Test
+    public void ArgsParameterizedConstructorTest() {
+        Args args = new Args("\"indubitably\"");
+        assertTrue(args.getArgsValue().equals("\"indubitably\""));
+    }
+
+    /**
+     * Test toString.
+     */
+    @Test
+    public void ArgsToStringTest() {
+        Args args = new Args("bant");
+        assertTrue(args.toString().equals("bant"));
+    }
+
 }
