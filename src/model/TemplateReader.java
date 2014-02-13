f60e3756b49e43af87ed33550bac62af3c0f17db
Ricky <rickygorowsky@email.arizona.edu>
2014-02-13 15:29:32 -0700

    Just starting Template reader task, but going to do code review first

---
+++ src/model/TemplateReader.java
@@ -0,0 +1,13 @@
+package model;
+
+public class TemplateReader {
+
+    public String read(Template template) {
+        
+    }
+    
+    public String read(String inputFile) {
+        Template template = getTemplate(inputFile); 
+    }
+    
+}
