571e889601e5cbfd9ecbe3dc3aee0e7250fc107e
Ricky <rickygorowsky@email.arizona.edu>
2014-02-28 12:31:54 -0700

    changing branches, still working on getting a solution

--- src/test/BogusWork.java
+++ src/test/BogusWork.java
@@ -2,9 +2,25 @@ package test;
 
 public class BogusWork {
     
-    public void someMethod(int param) {
+    public void dumb(Integer param) {
 
         int i = param;
+        while (true) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            System.out.println("in loop " + i);
+            i++;
+        }
+
+    }
+    
+    public void dumber() {
+
+        int i = 0;
         while (true) {
             try {
                 Thread.sleep(100);
--- src/test/JunkTest.java
+++ src/test/JunkTest.java
@@ -1,40 +1,76 @@
 package test;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
+import java.lang.reflect.Method;
 import org.junit.Test;
 
 public class JunkTest {
 
+    private Class<BogusWork> runningClass;
+    private Method runningMethod;
+    private Object[] args;
+    
+    public class ThreadStopper extends Thread {
+        @Override 
+        public void run() {
+            Class<BogusWork> theClass = runningClass;
+            Method theMethod = runningMethod;
+            
+            System.out.println(theClass.toString() + "::::" + theMethod.toString());
+            
+            args = new Object[] {new Integer(1)};
+            try {
+                theMethod.invoke(BogusWork.class, 0);
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+    
     
     @Test
     public void doingStuff() {
-        BogusWork ban = new BogusWork();
-        Thread get = new Thread() {
-            @Override
-            public void run() {
-                // do method
-            }
-        };
-        get.start();
-        System.out.println("here");
-        System.out.println("here bant");
-        System.out.println("here again");
+
+        
+        final BogusWork ban = new BogusWork();
+        runningClass = BogusWork.class;
         try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
-        System.out.println("before interupt");
-        get.stop();
-        System.out.println("after interupt");
+            runningMethod = runningClass.getMethod("dumber");
+        } catch (Exception e2) {
+            e2.printStackTrace();
+        } 
+        
+        System.out.println(runningClass.toString() + "::::" + runningMethod.toString());
+        
         try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
+            runningMethod.invoke(ban.getClass());
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
+        
+//        Thread get = new Thread() {
+//            @Override
+//            public void run() {
+//                ban.dumb(0);
+//            }
+//        };
+//        get.start();
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e1) {
+//            // TODO Auto-generated catch block
+//            e1.printStackTrace();
+//        }
+//        System.out.println("before interupt");
+//        get.stop();
+//        System.out.println("after interupt");
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+        System.out.println("end of prog");
     }
 }
