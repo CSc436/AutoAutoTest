86b1c9b6aaadd69ea7a5e5748df0416b88cce726
Ricky <rickygorowsky@email.arizona.edu>
2014-03-28 11:48:28 -0700

    changed resource file name, added blank lines

--- res/StudentSolutionSet1.java
+++ res/StudentSolutionSet1.java
@@ -2,20 +2,25 @@ public class StudentSolutionSet1 {
     public int add1(int num) {
         return num + 1;
     }
+    
     public String appendCom(String str) {
         return str + ".com";
     }
+    
     public boolean opposite(boolean val) {
         return !val;
     }
     
-    public String badAdd1(int num) {
-        return "" + (num + 1);
+    public int badAdd1(int num) {
+        return num - 1;
     }
+    
     public String badAppendCom(String str) {
         return str + ".gov";
     }
+    
     public boolean badOpposite(boolean val) {
         return val;
     }
+    
 }
