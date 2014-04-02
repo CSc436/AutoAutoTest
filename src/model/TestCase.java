e5a26f500fed03df39e8d5c8ef89e7273156c0d1
jeffersd <jeffersd@email.arizona.edu>
2014-04-02 15:57:58 -0700

    Changed the template to not check return value if the return value is void.

--- src/model/TestCase.java
+++ src/model/TestCase.java
@@ -265,6 +265,7 @@ public class TestCase {
                 ignoreWhitespace + "");
         template = template.replace("IS_IGNORE_PUNCTUATION", 
                 ignorePunctuation + "");
+        template = template.replace("IS_RETURN_VALUE_VOID", isVoid + "");
         return template;
     }
 }
